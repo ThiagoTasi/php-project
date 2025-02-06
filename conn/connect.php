994afefa9bc5171c48d735befebb8e994e30f3e6
Thiago Tasi <thiagosilvateo@gmail.com>
2025-02-06 17:27:25 -0300

    prep ambiente e index

---
+++ conn/connect.php
@@ -0,0 +1,18 @@
+<?php
+$host = "localhost";
+$database = "tinsphpdb01";
+$user = "root";
+$pass = "";
+$charset = "utf8";
+$port = "3306";
+ 
+ 
+try {
+    // lembre dessa variável quando usar um comando SQL no PHP
+    $conn = new mysqli($host,$user,$pass,$database,$port);
+    mysqli_set_charset($conn, $charset);
+}catch (Throwable $th){
+    die ("Atenção rolou um ERRO, Cara!".$th);
+}
+ 
+?>
--- index.php
+++ index.php
@@ -1,11 +1,37 @@
+<?php
+
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=,, initial-scale=1.0">
-    <title>Document</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootsstrap.min.css">
+    <link rel="stylesheet" href="css/estilo.css">
+    <title>Chuleta Quente Churrascaria</title>
 </head>
-<body>
-    
+
+<body class="fundofixo">
+    <!-- Area de menu -->
+    <?php include 'menu_publico.php' ?>
+    <a name="home">&</a>
+    <main class="container">
+        <!-- Area de carousel -->
+        <?php include 'carousel.php' ?>
+        <!-- Area de destaque -->
+        <a class="pt-6" name="destaques">&nbsp;</a>
+        <?php include 'produtos_destaque.php'; ?>
+        <!-- Area geral de produtos -->
+        <a class="pt-6" name="produtos">&nbsp;</a>
+        <?php include 'produtos_geral.php'; ?>
+        <!-- rodapé -->
+        <footer class="panel-footer" style="background:none;">
+            <?php include 'rodape.php'; ?>
+            <a name="contato"></a>
+        </footer>
+    </main>
+
 </body>
+
 </html>
